1aabbd3d8e9926e2c29263d464507438e53e2483
Brian Semiglia <brian.semiglia@gmail.com>
2016-11-03 18:49:00 -0400

    Fixes access issues.

--- NonCoalescingDispatchQueue/Classes/NonCoalescingDispatchQueue.swift
+++ NonCoalescingDispatchQueue/Classes/NonCoalescingDispatchQueue.swift
@@ -5,43 +5,47 @@
 //  Created by Brian Semiglia on 9/18/16.
 //
 
-final class NonCoalescingDispatchQueue {
-    
-    struct Event {
-        let delay: TimeInterval
-        let action: () -> Void
+final public class NonCoalescingDispatchQueue {
+  
+  public struct Event {
+    public let delay: TimeInterval
+    public let action: () -> Void
+    public init(delay: TimeInterval, action: @escaping () -> Void) {
+      self.delay = delay
+      self.action = action
     }
-    
-    var timer: CADisplayLink?
-    let start = Date()
-    var events = [Event]()
-    let completion: (() -> Void)?
-    
-    init(events: [Event], completion: (() -> Void)?) {
-        self.events = events
-        self.completion = completion
-        timer = CADisplayLink(
-            target: self,
-            selector: #selector(didFire(_:))
-        )
-        timer?.add(
-            to: .main,
-            forMode: .defaultRunLoopMode
-        )
+  }
+  
+  var timer: CADisplayLink?
+  let start = Date()
+  var events = [Event]()
+  let completion: (() -> Void)?
+  
+  public init(events: [Event], completion: (() -> Void)?) {
+    self.events = events
+    self.completion = completion
+    timer = CADisplayLink(
+      target: self,
+      selector: #selector(didFire(_:))
+    )
+    timer?.add(
+      to: .main,
+      forMode: .defaultRunLoopMode
+    )
+  }
+  
+  @objc func didFire(_ input: CADisplayLink) {
+    let since = Date().timeIntervalSince(start)
+    events
+      .filter{ since >= $0.delay }
+      .forEach {
+        $0.action()
     }
-    
-    @objc func didFire(_ input: CADisplayLink) {
-        let since = Date().timeIntervalSince(start)
-        events
-        .filter{ since >= $0.delay }
-        .forEach {
-            $0.action()
-        }
-        events = events.filter { since < $0.delay }
-        if events.count == 0 {
-            timer?.invalidate()
-            completion?()
-        }
+    events = events.filter { since < $0.delay }
+    if events.count == 0 {
+      timer?.invalidate()
+      completion?()
     }
-    
+  }
+  
 }
